f4f566b6020484262de2e377461781b9629d2a5b
Jakub Míšek <jmisek@gmail.com>
2020-08-14 11:15:21 +0200

    test added

--- tests/Devsense.PHP.Phar.Tests/UnitTest1.cs
+++ tests/Devsense.PHP.Phar.Tests/UnitTest1.cs
@@ -6,8 +6,12 @@ namespace Devsense.PHP.Phar.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void PhpUnitFiles()
         {
+            var phar = PharFile.OpenPharFile("phpunit.phar");
+            var manifest = phar.Manifest;
+
+            Assert.True(manifest.Entries.TryGetValue("php-timer/Duration.php", out var entry) && entry.IsFile);
         }
     }
 }
