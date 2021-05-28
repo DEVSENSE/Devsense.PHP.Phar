153abb6404fb42c29b5fddde56b9b9d42114675c
Jakub Míšek <jmisek@gmail.com>
2021-05-28 14:17:17 +0200

    netcoreapp3.1, phpstan test

--- tests/Devsense.PHP.Phar.Tests/UnitTest1.cs
+++ tests/Devsense.PHP.Phar.Tests/UnitTest1.cs
@@ -13,5 +13,14 @@ namespace Devsense.PHP.Phar.Tests
 
             Assert.True(manifest.Entries.TryGetValue("php-timer/Duration.php", out var entry) && entry.IsFile);
         }
+
+        [Fact]
+        public void PhpstanFiles()
+        {
+            var phar = PharFile.OpenPharFile("phpstan.phar");
+            var manifest = phar.Manifest;
+
+            Assert.True(manifest.Entries.TryGetValue("bin/phpstan", out var entry) && entry.IsFile);
+        }
     }
 }
