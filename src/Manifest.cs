aee10677559b70f4b86be8b27fb35a81a9b7df39
Jakub Míšek <jmisek@gmail.com>
2021-05-28 14:39:18 +0200

    phar with file entry and dir entry with the same name

--- src/Manifest.cs
+++ src/Manifest.cs
@@ -1,21 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 
 namespace Devsense.PHP.Phar
 {
+    #region EntryKey
+
+    [DebuggerDisplay("{Name}")]
+    public readonly struct EntryKey : IEquatable<EntryKey>
+    {
+        public string Name { get; }
+
+        public bool IsDir { get; }
+
+        public EntryKey(string name, bool isdir)
+        {
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.IsDir = isdir;
+        }
+
+        public static EntryKey File(string name) => new EntryKey(name, false);
+
+        public static EntryKey Directory(string name) => new EntryKey(name, true);
+
+        public override bool Equals(object obj) => obj is EntryKey key && Equals(key);
+
+        public override int GetHashCode() => StringComparer.OrdinalIgnoreCase.GetHashCode(Name) ^ (IsDir ? 0x3 : 0xf);
+
+        public bool Equals(EntryKey other) => string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase) && IsDir == other.IsDir;
+
+        public override string ToString() => Name;
+
+        /// <summary>
+        /// Implicit conversion to a file entry key.
+        /// </summary>
+        public static implicit operator EntryKey(string filename) => File(filename);
+    }
+
+    #endregion
+
     /// <summary>
     /// Represents PHAR manifest section.
     /// </summary>
     public sealed class Manifest
     {
+        #region Nested class: EntryKeyComparer
+
+        sealed class EntryKeyComparer : IEqualityComparer<EntryKey>
+        {
+            public static EntryKeyComparer Instance { get; } = new EntryKeyComparer();
+
+            private EntryKeyComparer() { }
+
+            public bool Equals(EntryKey x, EntryKey y) => x.Equals(y);
+
+            public int GetHashCode(EntryKey obj) => obj.GetHashCode();
+        }
+
+        #endregion
+
         private uint _entriesCount;
         private Version _manifestVersion;
         private uint _flags;
         private string _alias;
         private byte[] _metadata;
-        private Dictionary<string, Entry>/*!!*/_entries;
+        private Dictionary<EntryKey, Entry>/*!!*/_entries;
 
         /// <summary>
         /// Gets the PHAR version.
@@ -25,20 +76,21 @@ namespace Devsense.PHP.Phar
         /// <summary>
         /// Gets contained PHAR entries.
         /// </summary>
-        public Dictionary<string, Entry> Entries { get { return _entries; } }
+        public Dictionary<EntryKey, Entry> Entries { get { return _entries; } }
 
         /// <summary>
         /// Gets entry corresponding to given name (file name or directory name).
+        /// If there is both file and directory with the same name, file entry is returned.
         /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
         public Entry this[string name]
         {
             get
             {
-                Entry entry;
-                _entries.TryGetValue(name, out entry);
-                return entry;
+                return
+                    _entries.TryGetValue(EntryKey.File(name), out var entry) ||
+                    _entries.TryGetValue(EntryKey.Directory(name), out entry)
+                    ? entry
+                    : null;
             }
         }
 
@@ -47,11 +99,7 @@ namespace Devsense.PHP.Phar
         /// </summary>
         public Entry GetFileEntry(string name)
         {
-            Entry entry;
-            if (_entries.TryGetValue(name, out entry) && entry != null && entry.IsFile)
-                return entry;
-
-            return null;
+            return _entries.TryGetValue(EntryKey.File(name), out var entry) ? entry : null;
         }
 
         /// <summary>
@@ -72,7 +120,7 @@ namespace Devsense.PHP.Phar
         private void Initialize(BinaryReader reader)
         {
             if (reader == null)
-                throw new ArgumentNullException("reader");
+                throw new ArgumentNullException(nameof(reader));
 
             _entriesCount = reader.ReadUInt32();
             _manifestVersion = ParseManifestVersion(((uint)reader.ReadByte() << 8) | ((uint)reader.ReadByte()) & 0xfff0);
@@ -88,10 +136,10 @@ namespace Devsense.PHP.Phar
 
         private void/*!!*/InitializeEntries(BinaryReader reader)
         {
-            _entries = new Dictionary<string, Entry>((int)_entriesCount, StringComparer.OrdinalIgnoreCase);
-            List<Entry> list = new List<Entry>((int)_entriesCount);
+            _entries = new Dictionary<EntryKey, Entry>((int)_entriesCount, EntryKeyComparer.Instance);
 
-            bool supportsDir = _manifestVersion >= DirSupportVersion;
+            var list = new List<Entry>((int)_entriesCount);
+            var supportsDir = _manifestVersion >= DirSupportVersion;
 
             // read entries
             for (int entryindex = 0; entryindex < _entriesCount; entryindex++)
@@ -100,10 +148,13 @@ namespace Devsense.PHP.Phar
             }
 
             // read entries content
-            foreach (var entry in list)
+            for (int entryindex = 0; entryindex < list.Count; entryindex++)
             {
+                var entry = list[entryindex];
+
                 entry.InitializeContent(reader);
-                _entries.Add(entry.Name, entry);
+
+                _entries.Add(new EntryKey(entry.Name, isdir: !entry.IsFile), entry);
             }
         }
 
